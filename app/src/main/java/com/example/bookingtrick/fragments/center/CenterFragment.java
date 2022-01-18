d2d723d75f6687efea910f0132768bebcca834f0
Marderon123 <alexmonu2000@gmail.com>
2022-01-18 05:24:57 +0100

    Centers Booking

--- app/src/main/java/com/example/bookingtrick/fragments/center/CenterFragment.java
+++ app/src/main/java/com/example/bookingtrick/fragments/center/CenterFragment.java
@@ -12,6 +12,11 @@ import androidx.fragment.app.Fragment;
 import com.example.bookingtrick.R;
 import com.example.bookingtrick.main.Session;
 import com.example.bookingtrick.model.Center;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -19,6 +24,7 @@ public class CenterFragment extends Fragment {
 
     private ArrayList<Center> centerlist;
     private ListView listView;
+    private DatabaseReference myRef;
 
     public CenterFragment() {
     }
@@ -27,15 +33,38 @@ public class CenterFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         centerlist = Session.getCenterList();
 
-        View view = inflater.inflate(R.layout.activity_center_selection, null);
-        listView = view.findViewById(R.id.listCenter);
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        listView = view.findViewById(R.id.homeLayout);
 
-        /*
+
+        ArrayList<Integer> integers = new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
         CenterAdapter centerAdapter = new CenterAdapter(getActivity(), centerlist, this);
-        listView.setAdapter(centerAdapter);
+        int max = 0;
+        for (Integer i : integers) {
+            if (i == max) {
+                FirebaseDatabase.getInstance().getReference("Center/" + name.get(integers.indexOf(i)))
+                        .addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
 
-*/
+                                if (dataSnapshot.exists()) {
+                                    String name = dataSnapshot.child("name").getValue(String.class);
+                                    String street = dataSnapshot.child("street").getValue(String.class);
+                                    listView.setAdapter(centerAdapter);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                System.out.println("Fallo la lectura: " + databaseError.getCode());
+                            }
+                        });
+            }
+
+        }
 
         return view;
     }
 }
+
